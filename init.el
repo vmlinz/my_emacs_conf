df0605643865760261a62cf44589717aab6c1bd3
vmlinz <vmlinz@gmail.com>
2009-12-09 15:18:05 +0800

    add c & c++ mode hook to set default code style

--- init.el
+++ init.el
@@ -1,15 +1,27 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(column-number-mode t)
  '(tool-bar-mode nil)
  '(show-paren-mode t)
  '(uniquify-buffer-name-style (quote forward) nil (uniquify)))
+
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
+
+(defun my-c-mode-hook ()
+  (no-case-fold-search)
+  (c-set-style "linux")
+  )
+(add-hook 'c-mode-hook 'my-c-mode-hook)
+
+(defun my-c++-mode-hook ()
+  (c-set-style "stroustrup")
+)
+(add-hook 'c++-mode-hook 'my-c++-mode-hook)
