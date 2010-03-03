d34a9fa6e1459abed6505e2afba8f8fbf5254261
Zaicheng Qi <vmlinz@gmail.com>
2010-03-03 10:26:37 +0800

    delete init.elc from git and ignore it

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 ;; This file is not part of gnu emacs
-;; Time-stamp: <2010-03-03 10:18:12 vmlinz>
+;; Time-stamp: <2010-03-03 10:20:09 vmlinz>
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
