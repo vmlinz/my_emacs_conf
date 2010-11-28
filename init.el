0b232e5aa4aae3045f5961823a91f77fa14cdd88
Zaicheng Qi <vmlinz@gmail.com>
2010-11-28 19:48:53 +0800

    * init.el: cleanup emacs init code and make cc-mode initialization more structured

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 ;; This file is not part of gnu emacs
-;; Time-stamp: <2010-11-26 09:13:31 vmlinz>
+;; Time-stamp: <2010-11-27 00:57:01 vmlinz>
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
@@ -22,7 +22,7 @@
 ;; 4.The emacs i use is debian specifical, maybe I have to customize it through
 ;; debian source code
 ;; 5.tweak the third party lisp package configurations and make it more structured
-;; 6.speed up startup time by make defuns and hooks
+;; 6.speed up startup time by making defuns and hooks
 ;; 7.the configuration contains some machine specifical settings for Lenovo X200
 ;; 8.introduce the newly pacage manager _el-get_ to manage various third party
 ;; packages
@@ -54,7 +54,7 @@
   )
 (if (window-system)
   (add-hook 'after-make-frame-hook 'my-set-frame-font)
-)
+  )
 
 ;; encodings and locales
 (defun my-coding-system-init()
@@ -177,13 +177,9 @@
   (setq server-socket-file "/tmp/emacs1000/server")
   (unless (file-exists-p server-socket-file)
     (server-start))
-  ;; exit emacs client
+
   (defun my-exit-emacs-client ()
-    "consistent exit emacsclient.
-   if not in emacs client, echo a message in minibuffer, don't exit emacs.
-   if in server mode
-      and editing file, do C-x # server-edit
-      else do C-x 5 0 delete-frame"
+    "consistent exit emacsclient"
     (interactive)
     (if server-buffer-clients
       (server-edit)
@@ -217,7 +213,6 @@
 ;;########## end ##########
 
 ;;########## key bindings ##########
-;; buffer switching
 ;; some keybindings here are specifical to Lenovo Thinkpad
 (defun my-key-init()
   ;; skeleton pairs
@@ -291,24 +286,6 @@
 (my-org-mode-init)
 ;; ########## end ##########
 
-;; ########## notify ##########
-;; use libnotify and mplayer for sound
-(defun my-notify-send (title msg &optional icon sound)
-  "Show a popup if we're on X, or echo it otherwise; TITLE is the title
-of the message, MSG is the context. Optionally, you can provide an ICON and
-a sound to be played"
-
-  (interactive)
-  (when sound (shell-command
-		(concat "mplayer -really-quiet " sound " 2> /dev/null")))
-  (if (eq window-system 'x)
-    (shell-command (concat "notify-send "
-		     (if icon (concat "-i " icon) "")
-		     " '" title "' '" msg "'"))
-    ;; text only version
-    (message (concat title ": " msg))))
-;; ########## end ##########
-
 ;; ########## woman ##########
 ;; settings for woman
 (add-hook 'after-init-hook
@@ -324,160 +301,132 @@ a sound to be played"
 
 ;; ########## cc-mode ##########
 ;; c mode common hook
-(defun my-cc-mode-init()
-  (defun my-c-mode-common-hook()
-    ;; compilation
-    (add-hook 'compilation-finish-functions
-      '(lambda (buf str)
-	 (if (string-match "exited abnormally" str)
-	   (next-error)
-	   ;;no errors, make the compilation window go away in a few seconds
-	   (run-at-time "2 sec" nil 'delete-windows-on (get-buffer-create "*compilation*"))
-	   (message "No Compilation Errors!")
-	   )
-	 ))
-    (setq compilation-window-height 16)
-    (setq compilation-scroll-output t)
-
-    (defun my-do-compile()
-      (interactive)
-      (message (compile-command))
-      (compile (compile-command))
-      )
-
-    (defun my-do-lint()
-      (interactive)
-      (set (make-local-variable 'compile-command)
-	(let ((file (file-name-nondirectory buffer-file-name)))
-	  (format "%s %s %s"
-	    "splint"
-	    "+single-include -strict -compdef -nullpass -preproc +matchanyintegral -internalglobs -I/usr/include/gtk-2.0/ -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include -I/usr/include/cairo/"
-	    file
-	    )))
-      (compile compile-command)
-      )
-
-    (defun my-do-cdecl ()
-      (interactive)
-      (shell-command
-	(concat "cdecl explain \"" (buffer-substring (region-beginning)
-				     (region-end)) "\""))
-      )
-
-    ;; gdb
-    (add-hook 'gud-mode-hook
-      '(lambda()
-	 (setq gdb-show-main t)
-	 (setq gdb-many-windows -1)
-	 (define-key gud-mode-map [(f8)] 'gdb-many-windows)
-	 ))
-
-    (require 'xcscope)
-
-    (eval-after-load `xcscope
-      `(progn
-	 ;; cscope databases
-	 (setq cscope-database-regexps
-	   '(
-	      ( "/home/Projects/libc"
-		(t)
-		("/usr/src/linux/include")
-		)
-	      ))
-
-	 (setq cscope-do-not-update-database t)
-	 (setq cscope-display-cscope-buffer nil)
-	 (setq cscope-edit-single-match nil)))
-
-    ;; major key bindings for c-modes
-    (local-set-key "\C-c\C-c" 'comment-dwim)
-    (define-key c-mode-base-map [(return)] 'newline-and-indent)
-    (define-key c-mode-base-map [(f7)] 'compile)
-    (define-key c-mode-base-map [(f8)] 'gdb)
-    (define-key c-mode-base-map [(meta \')] 'c-indent-command)
-    )
-  (add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
-
-  (defun my-c-mode-hook ()
-    (c-set-style "linux")
-    ;; (c-set-offset arglist-cont-nonempty +)
-    ;; c preprocessing
-    (setq c-macro-shrink-window-flag t)
-    (setq c-macro-preprocessor "cpp")
-    (setq c-macro-cppflags " ")
-    (setq c-macro-prompt-flag t)
-    )
-  (add-hook 'c-mode-hook 'my-c-mode-hook)
-
-  (defun my-c++-mode-hook ()
-    (c-set-style "stroustrup")
-    )
-  (add-hook 'c++-mode-hook 'my-c++-mode-hook)
-  ;; ########## linux kernel coding style ##########
-  (defun c-lineup-arglist-tabs-only (ignored)
-    "Line up argument lists by tabs, not spaces"
-    (let* ((anchor (c-langelem-pos c-syntactic-element))
-	    (column (c-langelem-2nd-pos c-syntactic-element))
-	    (offset (- (1+ column) anchor))
-	    (steps (floor offset c-basic-offset)))
-      (* (max steps 1)
-	c-basic-offset)))
-
-  (add-hook 'c-mode-common-hook
-    '(lambda ()
-       ;; Add kernel style
-       (c-add-style
-	 "linux-tabs-only"
-	 '("linux" (c-offsets-alist
-		     (arglist-cont-nonempty
-		       c-lineup-gcc-asm-reg
-		       c-lineup-arglist-tabs-only))))))
-
-  (add-hook 'c-mode-hook
-    '(lambda ()
-       (let ((filename (buffer-file-name)))
-	 ;; Enable kernel mode for the appropriate files
-	 (when (and filename
-		 (string-match (expand-file-name "~/Projects/kernel")
-		   filename))
-	   (setq indent-tabs-mode t)
-	   (c-set-style "linux-tabs-only")))))
+(defun my-compile-init ()
+  "compile mode init function"
+  (add-hook 'compilation-finish-functions
+    '(lambda (buf str)
+       (if (string-match "exited abnormally" str)
+	 (next-error)
+	 ;;no errors, make the compilation window go away in a few seconds
+	 (run-at-time "2 sec" nil 'delete-windows-on
+	   (get-buffer-create "*compilation*"))
+	 (message "No Compilation Errors!")
+	 )
+       ))
+  (setq compilation-window-height 16)
+  (setq compilation-scroll-output t)
   )
-;; ########## end ##########
+
+(defun my-gud-init ()
+  "gud mode init function"
+  (add-hook 'gud-mode-hook
+    '(lambda()
+       (setq gdb-show-main t)
+       (setq gdb-many-windows -1)
+       (define-key gud-mode-map [(f8)] 'gdb-many-windows)
+       ))
+  )
+
+(defun my-cscope-init ()
+  "cscope emacs mode init function"
+  (require 'xcscope)
+  (eval-after-load `xcscope
+    `(progn
+       (setq cscope-do-not-update-database t)
+       (setq cscope-display-cscope-buffer nil)
+       (setq cscope-edit-single-match nil)))
+  )
+
+(defun my-c-mode-key-init ()
+  "c mode key bindings"
+  (local-set-key "\C-c\C-c" 'comment-dwim)
+  (define-key c-mode-base-map [(return)] 'newline-and-indent)
+  (define-key c-mode-base-map [(f7)] 'compile)
+  (define-key c-mode-base-map [(f8)] 'gdb)
+  (define-key c-mode-base-map [(meta \')] 'c-indent-command)
+  )
+
+(defun c-lineup-arglist-tabs-only (ignored)
+  "Line up argument lists by tabs, not spaces"
+  (let* ((anchor (c-langelem-pos c-syntactic-element))
+	  (column (c-langelem-2nd-pos c-syntactic-element))
+	  (offset (- (1+ column) anchor))
+	  (steps (floor offset c-basic-offset)))
+    (* (max steps 1)
+      c-basic-offset)))
+
+(defun my-c-linux-style-init ()
+  "add linux c style"
+  (c-add-style
+    "linux-tabs-only"
+    '("linux"
+       (c-offsets-alist
+	 (arglist-cont-nonempty
+	   c-lineup-gcc-asm-reg
+	   c-lineup-arglist-tabs-only))))
+  )
+
+(defun my-c-mode-init ()
+  (setq c-macro-shrink-window-flag t)
+  (setq c-macro-preprocessor "cpp")
+  (setq c-macro-cppflags " ")
+  (setq c-macro-prompt-flag t)
+  (setq indent-tabs-mode t)
+  (c-set-style "linux-tabs-only")
+  )
+
+(defun my-cc-mode-init()
+  (add-hook 'c-mode-common-hook 'my-compile-init)
+  (add-hook 'c-mode-common-hook 'my-gud-init)
+  (add-hook 'c-mode-common-hook 'my-cscope-init)
+  (add-hook 'c-mode-common-hook 'my-c-mode-key-init)
+  (add-hook 'c-mode-common-hook 'my-c-linux-style-init)
+
+  (add-hook 'c-mode-hook 'my-c-mode-init)
+  (add-hook 'c++-mode-hook
+    '(lambda ()
+       (c-set-style "stroustrup")
+       )
+    )
+  )
 (my-cc-mode-init)
 ;; ########## end ##########
 
 ;; ########## auctex ##########
-;; default to xelatex
-;; exec-path for texlive2009 from CTAN
+;; feature rich package for TeXing
 (defun my-auctex-init()
   (add-hook 'LaTeX-mode-hook
     '(lambda()
-       (setenv "PATH" (concat (getenv "PATH") ":/usr/local/texlive/2009/bin/x86_64-linux/"))
-       (add-to-list 'exec-path "/usr/local/texlive/2009/bin/x86_64-linux/")
-       (add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t))
+       (setenv "PATH"
+	 (concat (getenv "PATH")
+	   ":/usr/local/texlive/2009/bin/x86_64-linux/"))
+       (add-to-list 'exec-path
+	 '("/usr/local/texlive/2009/bin/x86_64-linux/"))
+       (add-to-list 'TeX-command-list
+	 '("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t))
        (setq TeX-command-default "XeLaTeX")
        (setq TeX-save-query nil)
        (setq TeX-show-compilation t)
        (setq Tex-master nil)
        )
     )
-  ;; set preview application and preview style
+
   (eval-after-load "tex"
     '(progn
        (TeX-global-PDF-mode t)
        (setq TeX-output-view-style
 	 (cons '("^pdf$" "." "acroread %o") TeX-output-view-style)
 	 )))
+
   (add-hook 'TeX-mode-hook
     '(lambda ()
        (turn-on-reftex)
        (auto-fill-mode)
        (outline-minor-mode)
        (flyspell-mode)))
+
   (add-hook 'LaTeX-mode-hook
     '(lambda ()
-       ;; minor modes
        (autoload 'reftex-mode "reftex" "RefTeX Minor Mode" t)
        (autoload 'turn-on-reftex "reftex" "RefTeX Minor Mode" nil)
        (autoload 'reftex-citation "reftex-cite" "Make citation" nil)
@@ -515,13 +464,7 @@ a sound to be played"
 ;; ########## end ##########
 
 ;; ########## cedet ##########
-;; cedet frome cvs configured for c/c++ programming
-;; cedet' stable functions is going to be integrited in emacs 23.2
-;; NOTE: cedet from cvs has more functions than the integrited one but it's not
-;; as clean as the one integrited
-;; (add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/cedet/common"))
-;; (require 'cedet)
-
+;; cedet is now managed by _el-get_ using latest source from cvs
 (defun my-semantic-hook ()
   "feature setting hook for semantic"
   (require 'semantic-ia)
@@ -529,17 +472,12 @@ a sound to be played"
 
   (imenu-add-to-menubar "TAGS")
   (global-ede-mode t)
-  ;; srecode-minor-mode
-  (srecode-minor-mode 1)
-  ;; semantic minor modes
+  (srecode-minor-mode t)
   (semantic-load-enable-code-helpers)
-  ;;(semantic-load-enable-gaudy-code-helpers)
+  (global-semantic-highlight-func-mode 1)
   (global-semantic-show-unmatched-syntax-mode -1)
   (global-semantic-tag-folding-mode -1)
-  ;; idle scheduler
   (global-semantic-idle-scheduler-mode -1)
-  ;;(global-semantic-decoration-mode 1)
-  (global-semantic-highlight-func-mode 1)
   (semantic-load-enable-all-exuberent-ctags-support)
 
   (setq-mode-local c-mode semanticdb-find-default-throttle
@@ -549,22 +487,22 @@ a sound to be played"
   )
 
 (defun my-semantic-key-hook ()
-  (local-set-key [(control return)] 'semantic-ia-complete-symbol-menu)
-  (local-set-key "\C-c/" 'semantic-ia-complete-symbol)
-  ;;
+  "key bindings for semantic modes"
   (local-set-key "\C-c>" 'semantic-complete-analyze-inline)
   (local-set-key "\C-c=" 'semantic-decoration-include-visit)
+  (local-set-key "\C-cp" 'semantic-analyze-proto-impl-toggle)
+  (local-set-key "\C-c\C-r" 'semantic-symref)
 
+  (local-set-key [(control return)] 'semantic-ia-complete-symbol-menu)
+  (local-set-key "\C-c/" 'semantic-ia-complete-symbol)
   (local-set-key "\C-cj" 'semantic-ia-fast-jump)
   (local-set-key "\C-cd" 'semantic-ia-show-doc)
   (local-set-key "\C-cm" 'semantic-ia-show-summary)
-  (local-set-key "\C-cp" 'semantic-analyze-proto-impl-toggle)
 
   (local-set-key "." 'semantic-complete-self-insert)
   (local-set-key ">" 'semantic-complete-self-insert)
   (local-set-key "\C-ct" 'eassist-switch-h-cpp)
   (local-set-key "\C-ce" 'eassist-list-methods)
-  (local-set-key "\C-c\C-r" 'semantic-symref)
   )
 
 (defun my-semanticdb-hook ()
@@ -577,16 +515,20 @@ a sound to be played"
 
 (defun my-semantic-qt4-hook()
   "qt4 setting hook for semantic"
-  ;; qt4 programming settings
   (defvar qt4-base-dir nil "Qt4 base include directory")
   (setq qt4-base-dir "/usr/include/qt4")
   (semantic-add-system-include qt4-base-dir 'c++-mode)
   (add-to-list 'auto-mode-alist (cons qt4-base-dir 'c++-mode))
-  (add-to-list 'semantic-lex-c-preprocessor-symbol-file (concat qt4-base-dir "/Qt/qconfig.h"))
-  (add-to-list 'semantic-lex-c-preprocessor-symbol-file (concat qt4-base-dir "/Qt/qconfig-dist.h"))
-  (add-to-list 'semantic-lex-c-preprocessor-symbol-file (concat qt4-base-dir "/Qt/qglobal.h"))
+  (add-to-list 'semantic-lex-c-preprocessor-symbol-file
+    (concat qt4-base-dir "/Qt/qconfig.h"))
+  (add-to-list 'semantic-lex-c-preprocessor-symbol-file
+    (concat qt4-base-dir "/Qt/qconfig-dist.h"))
+  (add-to-list 'semantic-lex-c-preprocessor-symbol-file
+    (concat qt4-base-dir "/Qt/qglobal.h"))
   )
+
 (defun my-semantic-init()
+  "all in one semantic init function"
   (add-hook 'semantic-init-hooks 'my-semantic-hook)
   (add-hook 'semantic-init-hooks 'my-semantic-key-hook)
   (add-hook 'semantic-init-hooks 'my-semanticdb-hook)
@@ -596,7 +538,6 @@ a sound to be played"
 
 ;; ########## jdee ##########
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/jdee/lisp"))
-;; defer loading jde until first open a java file
 (setq defer-loading-jde t)
 (if defer-loading-jde
   (progn
@@ -606,14 +547,13 @@ a sound to be played"
 	'(("\\.java\\'" . jde-mode))
 	auto-mode-alist)))
   (require 'jde))
-;; my jde-mode hook
+
 (defun my-jde-mode-hook ()
+  "my jde mode hook"
   (setq c-basic-offset 4)
-  ;; use java alternaltives in debian systems
   (setq jde-jdk-registry (quote
-			   (("default" . "/usr/lib/jvm/defaut-java")
-			     ("1.6" . "/usr/lib/jvm/java-6-sun")
-			     ))
+			   (("1.6" . "/usr/lib/jvm/java-6-sun"))
+			   )
     jde-jdk '("1.6")
     )
   )
@@ -648,19 +588,21 @@ a sound to be played"
 		   (concat ":" (expand-file-name android-mode-sdk-dir) "tools")
 		   (concat ":" (expand-file-name android-mode-ndk-dir))))
   ;; set sdk tools and ndk tools to emacs exec path
-  (add-to-list 'exec-path (expand-file-name (concat android-mode-sdk-dir "tools")))
-  (add-to-list 'exec-path (expand-file-name android-mode-ndk-dir))
+  (add-to-list 'exec-path
+    (expand-file-name (concat android-mode-sdk-dir "tools")))
+  (add-to-list 'exec-path
+    (expand-file-name android-mode-ndk-dir))
   )
 (my-android-mode-init)
 ;; ########## end ##########
 
 ;; ########## yasnippet ##########
-;; yasnippet
+;; yasnippet is now managed by _el-get_
 (defun my-yasnippet-init()
+  "simple yasnippet mode init function"
   (require 'yasnippet)
   (setq yas/root-directory "~/.emacs.d/el-get/yasnippet/snippets")
   (yas/load-directory yas/root-directory)
-  ;; set yas menu to abbreviate mode
   (setq yas/use-menu 'abbreviate)
   (yas/global-mode t)
   )
@@ -668,28 +610,28 @@ a sound to be played"
 
 ;; ########## auto-complete ##########
 ;; this package is good to use and easy to config
-;; now without cedet semantic support, it will be add next
 (defun my-auto-complete-init()
+  "auto-complete init function"
   (require 'auto-complete-config)
-  ;; ac default configuration
   (ac-config-default)
-  ;; ac customizations
+  (setq ac-dwim t)
+  (setq ac-auto-start 3)
+  (setq ac-use-quick-help nil)
+
   (set-face-background 'ac-candidate-face "lightgray")
   (set-face-underline-p 'ac-candidate-face "darkgray")
   (set-face-background 'ac-selection-face "steelblue")
+
   (define-key ac-completing-map "\M-n" 'ac-next)
   (define-key ac-completing-map "\M-p" 'ac-previous)
-  (setq ac-dwim t)
   (define-key ac-mode-map [(C-tab)] 'auto-complete)
-  ;;start completion when entered 3 characters
-  (setq ac-auto-start 3)
-  (setq ac-use-quick-help nil)
   )
 ;; ########## end ##########
 
 ;; ########## git ##########
-;; configure magit
+;; configure magit package for git support
 (defun my-git-init()
+  "magit init function for el-get"
   (autoload 'magit-status "magit" nil t)
   (global-set-key (kbd "C-x C-z") 'magit-status)
   (eval-after-load 'magit
