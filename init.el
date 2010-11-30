5e629e1798fb00317542e8b3ebf917f697af5ea7
Zaicheng Qi <vmlinz@gmail.com>
2010-11-30 14:18:22 +0800

    * init.el: rebind set-mark-command

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 ;; This file is not part of gnu emacs
-;; Time-stamp: <2010-11-30 13:54:08 vmlinz>
+;; Time-stamp: <2010-11-30 14:17:12 vmlinz>
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
@@ -226,8 +226,11 @@
   (global-set-key [(XF86Back)] 'previous-buffer)
   (global-set-key (kbd "C-x n") 'next-buffer)
   (global-set-key [(XF86Forward)] 'next-buffer)
+  ;; comment-dwim
   (global-set-key "\C-c\C-c" 'comment-dwim)
   (global-set-key "\C-x\C-b" 'ibuffer)
+  ;; set mark
+  (global-set-key "\C-m" 'set-mark-command)
   )
 (my-key-init)
 ;; ########## end ##########
