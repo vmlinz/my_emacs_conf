f4caab4f3bddd9fce15110a7dae2431ed98d6dcb
Zaicheng Qi <vmlinz@gmail.com>
2010-12-02 20:01:52 +0800

    * init.el: configure ac-source-clang with gtk support

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 ;; This file is not part of gnu emacs
-;; Time-stamp: <2010-12-02 14:12:13 vmlinz>
+;; Time-stamp: <2010-12-02 20:00:46 vmlinz>
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
@@ -592,6 +592,9 @@
     '(lambda ()
        (setq ac-sources
 	 (append '(ac-source-clang) ac-sources))
+       (setq ac-clang-flags
+	 (split-string
+	   (shell-command-to-string "pkg-config --cflags gtk+-2.0")))
        )
     )
   )
@@ -622,7 +625,8 @@
   (define-key ac-menu-map "\M-n" 'ac-next)
   (define-key ac-menu-map "\C-p" 'ac-previous)
   (define-key ac-menu-map "\M-p" 'ac-previous)
-  (define-key ac-mode-map "\M-/" 'auto-complete)
+  (define-key ac-menu-map "\M-/" 'auto-complete)
+  (define-key ac-menu-map "\C-f" 'ac-stop)
   )
 ;; ########## end ##########
 
