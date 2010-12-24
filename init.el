c0580518b64d238ad768e8aee75bfa8302ddfad7
Zaicheng Qi <vmlinz@gmail.com>
2010-12-24 13:19:45 +0800

    -- set add .zsh file to auto-mode-alist -- add java customizations

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 ;; This file is not part of gnu emacs
-;; Time-stamp: <2010-12-17 17:28:43 vmlinz>
+;; Time-stamp: <2010-12-24 13:10:52 vmlinz>
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
@@ -367,6 +367,13 @@
        (c-set-style "stroustrup")
        )
     )
+  (add-hook 'java-mode-hook
+    '(lambda ()
+       (setq indent-tabs-mode nil)
+       (c-set-offset 'arglist-cont-nonempty '+)
+       (c-set-offset 'topmost-intro-cont 0)
+       )
+    )
   )
 (my-cc-mode-init)
 ;; ########## end ##########
@@ -429,12 +436,22 @@
 ;; ########## markdown ##########
 ;; markdown-mode for translating Pro Git
 (defun my-markdown-mode-init()
-  (autoload 'markdown-mode "markdown-mode.el"
+  (autoload 'markdown-mode "markdown-mode"
     "Major mode for editing Markdown files" t)
   (setq auto-mode-alist
-    (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
+    (cons '("\\.markdown\\'" . markdown-mode) auto-mode-alist))
   )
 (my-markdown-mode-init)
+;; ########## end ##########
+
+;; ########## sh-mode ##########
+(defun my-sh-mode-init()
+  (autoload 'sh-mode "sh-script"
+    "Major mode for editing shell files" t)
+  (setq auto-mode-alist
+    (cons '("\\.zsh\\'" . sh-mode) auto-mode-alist))
+  )
+(my-sh-mode-init)
 ;; ########## end ##########
 
 ;; ########## lisp settings ##########
